8b8ef0b6af1f2b2e10e780865cb3abed987ea0ff
jmo <jmo@genesisrg.com>
2017-03-20 12:55:32 -0400

    third message

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 echo "this is the index page of this Git Repo.";
 echo "changed";
+//https://www.linux.com/learn/beginning-git-and-github-linux-users
+//https://www.howtoforge.com/tutorial/install-git-and-github-on-ubuntu-14.04/
 ?>
 
